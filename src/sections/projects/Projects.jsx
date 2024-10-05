29685c347a03a87a8b6de6ef9c370ba19ce2d444
Daniella Bosc <daniellabosc@gmail.com>
2024-10-05 15:19:35 -0700

    add tabs

--- src/sections/projects/Projects.jsx
+++ src/sections/projects/Projects.jsx
@@ -1,4 +1,5 @@
 import './projects.css'
+import data from '../projects/data'
 import Tabs from '../tabsList/TabsList'
 
 const Projects = () => {
@@ -6,6 +7,22 @@ const Projects = () => {
     <section id="projects" data-aos="fade-in">
         <div className="projects__container">
             <Tabs />
+        </div>
+        <div className="container projects__responsive">
+            {data.map((item)=>{
+                return (
+                    <div className="container projects__card">
+                   <p>{item.job} - {item.title}</p>
+                   <span  className="projects__cloud">
+                    {item.tech.map((i)=>{
+                        return (
+                            <span>{i}</span>
+                        )
+                    })}
+                   </span>
+                   </div>
+                )
+            })}
         </div>
     </section>
   )
