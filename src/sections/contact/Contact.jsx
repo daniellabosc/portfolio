c88b8dbfed19ad57c751e8f38bd7a3735ae72b9a
Daniella Bosc <daniellabosc@gmail.com>
2024-10-05 14:40:16 -0700

    add tabs

--- src/sections/contact/Contact.jsx
+++ src/sections/contact/Contact.jsx
@@ -4,17 +4,17 @@ import "./contact.css";
 const Contact = () => {
   return (
     <section id="contact">
-      <h2>Get In Touch</h2>
-      <p>Shoot me a message via any of the links below!</p>
       <div className="container contact__container" data-aos="fade-up">
         {data.map((item) => (
+          <div>
           <a href={item.desc} className="btn primary">
             {item.icon}
             {item.title}
+          
           </a>
+          </div>
         ))}
       </div>
-      {/* </div> */}
     </section>
   );
 };
