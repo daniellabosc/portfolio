2bcd6bb1eeb80ee3a37a9e636150b00620172ec1
Daniella Bosc <daniellabosc@gmail.com>
2024-10-11 05:13:03 -0700

    updates

--- src/sections/summary/data.js
+++ src/sections/summary/data.js
@@ -1,7 +1,7 @@
 
 const data = ["Frontend work with React",
 "Frontend work with Vue, Angular",
-"Responsive ebsite layout/design with HTML and CSS",
+"Responsive UI layout/design with HTML and CSS",
 "Proficiency with Bootstrap",
 "Backend work with Node, PHP",
 "SQL databases, Mongo and Athena",
