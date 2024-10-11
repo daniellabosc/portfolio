ac3e4aa38350fd7e6f9631832fc7d331ccaa4f21
Daniella Bosc <daniellabosc@gmail.com>
2024-10-11 13:05:36 -0700

    updates

--- src/sections/projects/Projects.jsx
+++ src/sections/projects/Projects.jsx
@@ -5,22 +5,24 @@ import Tabs from '../tabsList/TabsList'
 const Projects = () => {
   return (
     <section id="projects" data-aos="fade-in">
-        <div className="projects__container">
+        {/* <div className="projects__container">
             <Tabs />
-        </div>
-        <div className="container projects__responsive">
+        </div> */}
+        <div className="fluid-container projects__responsive">
             {data.map((item)=>{
                 return (
                     <div className="container projects__card">
                    <h5>{item.job} - {item.title}</h5>
-                   <blockquote>{item.desc}</blockquote>
-                   <span  className="projects__cloud">
+                   <ul>
+                   {item.points.map((i)=><li key={i}>{i}</li>)}
+                   </ul>
+                   <div  className="projects__cloud">
                     {item.tech.map((i)=>{
                         return (
                             <span><strong>{i}</strong></span>
                         )
                     })}
-                   </span>
+                   </div>
                    </div>
                 )
             })}
--- src/sections/summary/data.js
+++ src/sections/summary/data.js
@@ -7,7 +7,7 @@ const data = ["Frontend work with React",
 "SQL databases, Mongo and Athena",
 "Experience consuming and building API's",
 "Comfortable working in a collaborative team / Agile workflow",
-"Proficiency with Git, Jira and TFS",
+"Proficiency with AWS, Git, Jira and TFS",
 "Strong analytical and problem solving skills",
 "Well designed, testable industry standard code"]
 
